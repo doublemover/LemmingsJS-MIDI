627eb8be8eba06f0d3ae99d12587a09d2ea6554f
doublemover <153689082+doublemover@users.noreply.github.com>
2025-05-14 19:17:41 -0400

    floating crash fix

--- js/ActionFloatingSystem.js
+++ js/ActionFloatingSystem.js
@@ -5,6 +5,8 @@ class ActionFloatingSystem {
         this.sprite = [];
         this.sprite.push(sprites.getAnimation(Lemmings.SpriteTypes.UMBRELLA, false));
         this.sprite.push(sprites.getAnimation(Lemmings.SpriteTypes.UMBRELLA, true));
+        this.floatSpeed = [3, 3, 3, 3, -1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2];
+        this.floatFrame = [0, 1, 3, 5, 5, 5, 5, 5, 5, 6, 7, 7, 6, 5, 4, 4];
     }
     getActionName() {
         return "floating";
@@ -19,16 +21,16 @@ class ActionFloatingSystem {
     /** render Lemming to gamedisplay */
     draw(gameDisplay, lem) {
         let ani = this.sprite[(lem.lookRight ? 1 : 0)];
-        let frame = ani.getFrame(ActionFloatingSystem.floatFrame[lem.frameIndex]);
+        let frame = ani.getFrame(this.floatFrame[lem.frameIndex]);
         gameDisplay.drawFrame(frame, lem.x, lem.y);
     }
     process(level, lem) {
         lem.frameIndex++;
-        if (lem.frameIndex >= ActionFloatingSystem.floatFrame.length) {
+        if (lem.frameIndex >= this.floatFrame.length) {
             /// first 8 are the opening of the umbrella
             lem.frameIndex = 8;
         }
-        let speed = ActionFloatingSystem.floatSpeed[lem.frameIndex];
+        let speed = this.floatSpeed[lem.frameIndex];
         for (let i = 0; i < speed; i++) {
             if (level.hasGroundAt(lem.x, lem.y + i)) {
                 // landed
