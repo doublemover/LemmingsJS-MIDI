4c53bee380cff472a7015889e776b17bbcf59e37
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 18:12:05 -0400

    Add depcheck script and document usage

--- tools/check-undefined.js
+++ tools/check-undefined.js
@@ -3,9 +3,8 @@ import path from 'path';
 import { spawnSync } from 'child_process';
 import { parse } from 'acorn';
 import { createRequire } from 'module';
-import { parse } from 'acorn';
-import { createRequire } from 'module';
 import { processHtmlFile as extractHtmlSnippets } from './processHtmlFile.js';
+import { DomUtils, parseDocument } from 'htmlparser2';
 
 const require = createRequire(import.meta.url);
 
