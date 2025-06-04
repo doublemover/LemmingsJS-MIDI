e63c4b738c31748746f2595bf047c8cc3a6011f3
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 17:02:07 -0400

    Add invalid bit count tests

--- test/bitreader.test.js
+++ test/bitreader.test.js
@@ -20,4 +20,19 @@ describe('BitReader', function() {
     expect(reader.getCurrentChecksum()).to.equal(0xFF);
     expect(reader.eof()).to.equal(true);
   });
+
+  it('throws for invalid bit counts without affecting checksum', function() {
+    const bytes = new Uint8Array([0xAA, 0x55]);
+    const bin = new BinaryReader(bytes);
+    const reader = new BitReader(bin, 0, bin.length);
+
+    expect(() => reader.read(0)).to.throw(RangeError);
+    expect(() => reader.read(33)).to.throw(RangeError);
+
+    const v1 = reader.read(8);
+    expect(v1).to.equal(0xAA);
+    const v2 = reader.read(8);
+    expect(v2).to.equal(0x55);
+    expect(reader.getCurrentChecksum()).to.equal(0xFF);
+  });
 });
