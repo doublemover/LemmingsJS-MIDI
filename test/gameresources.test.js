4aabd6eda6a5ddd2528880a33f374129a16ae36d
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 19:02:13 -0400

    feat: flash death dots on minimap

--- test/gameresources.test.js
+++ test/gameresources.test.js
@@ -109,7 +109,6 @@ describe('GameResources', function () {
     await gr.getCursorSprite();
     await gr.getMasks();
     assert.strictEqual(loadCount, 1);
-    assert.deepStrictEqual(partIndices, [0, 2, 6, 5, 1]);
     assert.deepStrictEqual(partIndices, [0, 2, 6, 1]);
   });
 });
