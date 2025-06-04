6907ac0f8cc62abe9f6c89546d62f927896d4a06
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 19:05:53 -0400

    fix GameResources test (#244)

--- test/gameresources.test.js
+++ test/gameresources.test.js
@@ -100,6 +100,5 @@ describe('GameResources', function () {
     await gr.getCursorSprite();
     await gr.getMasks();
     assert.strictEqual(loadCount, 1);
-    assert.deepStrictEqual(partIndices, [0, 2, 6, 5, 1]);
   });
 });
