dc3b114ebb9363f690784b2c2c21132f0a12e7a1
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 12:48:18 -0400

    Merge master and clean duplicates

--- js/ActionClimbSystem.js
+++ js/ActionClimbSystem.js
@@ -1,13 +1,26 @@
 import { Lemmings } from './LemmingsNamespace.js';
 import { ActionBaseSystem } from './ActionBaseSystem.js';
+    
 class ActionClimbSystem extends ActionBaseSystem {
+
+    constructor(sprites) {
         super({ sprites, spriteType: Lemmings.SpriteTypes.CLIMBING, actionName: 'climbing' });
+    }
+
+    triggerLemAction(lem) {
+        if (lem.canClimb) {
             return false;
         }
         lem.canClimb = true;
         return true;
     }
 
+    process(level, lem) {
+        lem.frameIndex = (lem.frameIndex + 1) % 8;
+        if (lem.frameIndex < 4) {
+            // check for top
+            if (!level.hasGroundAt(lem.x, lem.y - lem.frameIndex - 7)) {
+                lem.y = lem.y - lem.frameIndex + 2;
                 return Lemmings.LemmingStateType.HOISTING;
             }
             return Lemmings.LemmingStateType.NO_STATE_TYPE;
