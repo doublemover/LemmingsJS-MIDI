52d06ecd1be197cebfc36d5a3a79cc540dc3ec03
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 12:48:34 -0400

    chore: inline LogHandler in modules and tests

--- js/VGASpecReader.js
+++ js/VGASpecReader.js
@@ -1,4 +1,5 @@
 import { Lemmings } from './LemmingsNamespace.js';
+import './LogHandler.js';
 
 /**
  * Decodes a VGASPEC-format ground file, including color palette and image buffer.
--- test/levelwriter.test.js
+++ test/levelwriter.test.js
@@ -1,10 +1,17 @@
 import { expect } from 'chai';
 import { readFileSync } from 'fs';
-import '../js/LemmingsBootstrap.js';
 import { BinaryReader } from '../js/BinaryReader.js';
+import { BitReader } from '../js/BitReader.js';
+import { BitWriter } from '../js/BitWriter.js';
+import { UnpackFilePart } from '../js/UnpackFilePart.js';
 import { FileContainer } from '../js/FileContainer.js';
 import { LevelReader } from '../js/LevelReader.js';
 import { LevelWriter } from '../js/LevelWriter.js';
+import '../js/LevelProperties.js';
+import '../js/LevelElement.js';
+import '../js/DrawProperties.js';
+import '../js/Range.js';
+import '../js/SkillTypes.js';
 
 globalThis.lemmings = { game: { showDebug: false } };
 
