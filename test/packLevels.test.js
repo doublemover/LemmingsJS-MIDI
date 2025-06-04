5ff74b187736469c0c3f3f353b0db13e50094724
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 13:00:10 -0400

    Add integration tests for packLevels and patchSprites (#85)

---
+++ test/packLevels.test.js
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
+import { Lemmings } from '../js/LemmingsNamespace.js';
+import '../js/LogHandler.js';
+import { BinaryReader } from '../js/BinaryReader.js';
+import { BitReader } from '../js/BitReader.js';
+import { BitWriter } from '../js/BitWriter.js';
+import { PackFilePart } from '../js/PackFilePart.js';
+import '../js/UnpackFilePart.js';
+import { FileContainer } from '../js/FileContainer.js';
+
+globalThis.lemmings = { game: { showDebug: false } };
+
+describe('tools/packLevels.js', function () {
+  it('packs a directory of levels into a DAT file', async function () {
+    const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'levels-'));
+    const a = Uint8Array.from({ length: 2048 }, (_, i) => i % 256);
+    const b = Uint8Array.from({ length: 2048 }, (_, i) => (255 - i) % 256);
+    fs.writeFileSync(path.join(dir, 'A.bin'), a);
+    fs.writeFileSync(path.join(dir, 'B.bin'), b);
+    const outFile = path.join(dir, 'out.dat');
+
+    const origArgv = process.argv;
+    process.argv = ['node', 'packLevels.js', dir, outFile];
+    await import('../tools/packLevels.js');
+    process.argv = origArgv;
+
+    const buf = fs.readFileSync(outFile);
+    const fc = new FileContainer(new BinaryReader(new Uint8Array(buf)));
+    expect(fc.count()).to.equal(2);
+
+    const inputs = [a, b];
+    fc.parts.forEach((part, idx) => {
+      const unpacked = fc.getPart(idx);
+      expect(Array.from(unpacked.data.slice(0, unpacked.length)))
+        .to.eql(Array.from(inputs[idx]));
+      const packed = PackFilePart.pack(inputs[idx]);
+      expect(part.checksum).to.equal(packed.checksum);
+      expect(part.initialBufferLen).to.equal(packed.initialBits);
+    });
+  });
+});
