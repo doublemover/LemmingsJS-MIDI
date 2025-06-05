cdb8578ba79738257c42ef86ef8760221f972ddb
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-05 00:12:41 -0400

    test walk collision

--- js/ActionWalkSystem.js
+++ js/ActionWalkSystem.js
@@ -30,6 +30,7 @@ class ActionWalkSystem extends ActionBaseSystem {
   }
   process(level, lem) {
     lem.frameIndex++;
+    const prevX = lem.x;
     lem.x += (lem.lookRight ? 1 : -1);
 
     const groundMask = level.getGroundMaskLayer();
@@ -37,6 +38,7 @@ class ActionWalkSystem extends ActionBaseSystem {
     const upDelta = this.getGroundStepHeight(stepSlice);
     if (upDelta == 8) {
       // collision with obstacle
+      lem.x = prevX; // revert movement into wall
       if (lem.canClimb) {
         return Lemmings.LemmingStateType.CLIMBING;
       } else {
