addaed2e8d550bec143c4da7a2f25baeb2695408
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-04 22:29:18 -0400

    Center GUI panel based on display width (#302)

---
+++ test/stage.updateStageSize.test.js
@@ -0,0 +1,72 @@
+import { expect } from 'chai';
+import { Lemmings } from '../js/LemmingsNamespace.js';
+import '../js/EventHandler.js';
+import '../js/Position2D.js';
+import '../js/ViewPoint.js';
+import '../js/StageImageProperties.js';
+import '../js/DisplayImage.js';
+import '../js/UserInputManager.js';
+import { Stage } from '../js/Stage.js';
+
+function createStubCanvas(width = 800, height = 600) {
+  const ctx = {
+    canvas: { width, height },
+    fillRect() {},
+    drawImage() {},
+    putImageData() {}
+  };
+  return {
+    width,
+    height,
+    getContext() { return ctx; },
+    addEventListener() {},
+    removeEventListener() {}
+  };
+}
+
+function createDocumentStub() {
+  return {
+    createElement() {
+      const ctx = {
+        canvas: {},
+        fillRect() {},
+        drawImage() {},
+        putImageData() {},
+        createImageData(w, h) {
+          return { width: w, height: h, data: new Uint8ClampedArray(w * h * 4) };
+        }
+      };
+      return {
+        width: 0,
+        height: 0,
+        getContext() { ctx.canvas = this; return ctx; }
+      };
+    }
+  };
+}
+
+globalThis.lemmings = { game: { showDebug: false } };
+
+describe('Stage.updateStageSize', function() {
+  before(function() {
+    global.document = createDocumentStub();
+  });
+
+  it('centers GUI panel after canvas resize', function() {
+    const canvas = createStubCanvas(400, 600);
+    const stage = new Stage(canvas);
+    stage.clear = () => {};
+    stage.draw = () => {};
+
+    const display = stage.getGuiDisplay();
+    display.initSize(160, 20);
+
+    canvas.width = 800;
+    canvas.getContext().canvas.width = 800;
+    stage.updateStageSize();
+
+    const guiW = display.getWidth() * stage.guiImgProps.viewPoint.scale;
+    expect(stage.guiImgProps.x).to.equal(Math.floor((800 - guiW) / 2));
+    expect(stage.guiImgProps.width).to.equal(display.getWidth());
+  });
+});
