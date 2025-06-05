1b49d7e90d2699bed41843c7c9d86b16486025aa
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-05 00:34:44 -0400

    load steel sprites early (#331)

--- tools/exportAllSprites.js
+++ tools/exportAllSprites.js
@@ -39,6 +39,7 @@ function frameToPNG(frame) {
   fs.mkdirSync(outDir, { recursive: true });
 
   const provider = new NodeFileProvider('.');
+  await Lemmings.loadSteelSprites();
   const res = new Lemmings.GameResources(provider, { path: dataPath, level: { groups: [] }});
   const pal = new Lemmings.ColorPalette();
 
@@ -47,10 +48,10 @@ function frameToPNG(frame) {
 
   const panel = panelSprites.getPanelSprite();
   await new Promise(res =>
-    frameToPNG(panel)
-      .pack()
+    frameToPNG(panel).pack()
       .pipe(fs.createWriteStream(`${outDir}/panel.png`))
-      .on('finish', res));
+      .on('finish', res)
+  );
 
   const letters = ['%', '0','1','2','3','4','5','6','7','8','9','-','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '];
   for (const letter of letters) {
