1834e6d6374fcb436d085e1b9b814e6b4ab0b8e3
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-06 14:59:52 -0400

    fix bench speed timing (#484)

--- js/GameTimer.js
+++ js/GameTimer.js
@@ -78,6 +78,8 @@ class GameTimer {
     }
   }
 
+  get frameTime() { return this.#frameTime; }
+
   #updateFrameTime() {
     this.#frameTime = this.TIME_PER_FRAME_MS / this.#speedFactor;
   }
@@ -203,6 +205,7 @@ class GameTimer {
 
     const diff = this.#speedFactor - oldSpeed;
     if (diff !== 0) {
+      this.#updateFrameTime();
       const intensity = Math.min(Math.abs(diff) / 5, 1);
       const color = diff > 0 ? `rgba(0,255,0,${intensity})` : `rgba(255,0,0,${intensity})`;
       const stage = lemmings?.stage;
