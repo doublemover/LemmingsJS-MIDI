0c0447f6290aba047d71d879728daa9139ae40a6
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-06 10:38:20 -0400

    Add custom crosshair cursor (#428)

---
+++ js/CrosshairCursor.js
@@ -0,0 +1,22 @@
+import { Lemmings } from './LemmingsNamespace.js';
+
+function createCrosshairFrame(size = 24) {
+  const frame = new Lemmings.Frame(size, size);
+  const center = Math.floor(size / 2);
+  const cw = Lemmings.ColorPalette.colorFromRGB(144, 238, 144); // light green
+  const ccw = Lemmings.ColorPalette.colorFromRGB(255, 255, 255); // white
+
+  for (let y = 0; y < size; y++) {
+    frame.setPixel(center, y, cw);
+    frame.setPixel(center - 1, y, ccw);
+  }
+
+  for (let x = 0; x < size; x++) {
+    frame.setPixel(x, center, cw);
+    frame.setPixel(x, center - 1, ccw);
+  }
+
+  return frame;
+}
+Lemmings.createCrosshairFrame = createCrosshairFrame;
+export { createCrosshairFrame };
