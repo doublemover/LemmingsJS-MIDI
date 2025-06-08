147558227327090296a24bf43b1d906051dab2ed
2xmvr <153689082+doublemover@users.noreply.github.com>
2025-06-08 16:50:48 -0400

    Update starting viewpoint (#747)
    * fix level view position
    
    * Auto-merge repo metrics and agent info
    
    * actually fixed
    
    * Auto-merge repo metrics and agent info
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- js/GameView.js
+++ js/GameView.js
@@ -481,9 +481,12 @@ class GameView extends Lemmings.BaseLogger {
       gameDisplay.clear();
       this.stage.resetFade();
       level.render(gameDisplay);
-      gameDisplay.setScreenPosition(level.screenPositionX, 0);
       this.stage.updateStageSize();
-      gameDisplay.redraw();
+      await this.start();
+
+      const stageImage = this.stage.gameImgProps;
+      this.stage.applyViewport(stageImage, level.screenPositionX, 0, 4);
+      this.stage.redraw();
     }
     this.updateQuery();
     this.log.debug(level);
